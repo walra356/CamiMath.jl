9e2f4b961669a7a470c1c8a0504adc9f63638826
Documenter.jl <documenter@juliadocs.github.io>
2023-01-30 22:25:49 +0000

    build based on afd4585

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CamMath.jl","page":"CamMath.jl","title":"CamMath.jl","text":"","category":"section"},{"location":"","page":"CamMath.jl","title":"CamMath.jl","text":"Documentation for CamMath.jl","category":"page"}]
+}
