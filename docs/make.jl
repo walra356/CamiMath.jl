f0ca1644d2683b5392601b665ac06e38bb47d64a
Jook Walraven <walra356@planet.nl>
2023-01-31 15:34:31 +0100

    patch make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,22 @@
 using Documenter
 using CamMath
 
-makedocs(
-    sitename = "CamMath",
-    format = Documenter.HTML(),
-    modules = [CamMath]
+makedocs(;
+    modules=[CamMath],
+    authors="= <walra356@planet.nl> and contributors",
+    repo="https://github.com/walra356/CamMath.jl/blob/{commit}{path}#L{line}",
+    sitename="CamMath.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://walra356.github.io/CamMath.jl",
+        assets=String[]
+    ),
+    pages=[
+        "Home" => "index.md",
+    ]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-deploydocs(
-    repo = "github.com/walra356/CamMath.jl.git"
+deploydocs(;
+    repo="github.com/walra356/CamMath.jl",
+    devbranch="main"
 )
