5eab7b0a4fa3be44fe802804a997f4c32b9fb779
Jook Walraven <walra356@planet.nl>
2023-02-01 16:55:13 +0100

    v3

--- docs/make.jl
+++ docs/make.jl
@@ -2,12 +2,12 @@ using Documenter
 using CamMath
 
 makedocs(;
-    modules=[CamMath],
-    authors="<walra356@planet.nl> and contributors",
-    #repo="https://github.com/walra356/CamMath.jl/blob/{commit}{path}#L{line}",
-    sitename="CamMath.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
+    modules = [CamMath],
+    authors = "<walra356@planet.nl> and contributors",
+    #repo = "github.com/walra356/CamMath.jl.git",
+    sitename = "CamMath.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
         #canonical="https://walra356.github.io/CamMath.jl",
         assets=String[]
     ),
@@ -17,6 +17,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/walra356/CamMath.jl.git",
-    devbranch="main"
+    repo = "github.com/walra356/CamMath.jl.git",
+    devbranch = "main"
 )
