1ff42390c48bd42d266b1e9f9abfc515d850eb82
Jook Walraven <walra356@planet.nl>
2023-02-01 23:31:04 +0100

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,7 @@ using Test
 
 @testset "CamMath.jl" begin
 
-    bernoulliB(60; msg=false)
-
-    @test bernoulliB_array(8) == [1 // 1, -1 // 2, 1 // 6, 0 // 1, -1 // 30, 0 // 1, 1 // 42, 0 // 1, -1 // 30] 
+    @test bernoulliB_array(10) == [1 // 1, -1 // 2, 1 // 6, 0 // 1, -1 // 30, 0 // 1, 1 // 42, 0 // 1, -1 // 30, 0 // 1, 5 // 66] 
     @test bernoulliB(0) == 1 // 1
     @test bernoulliB(1) == -1 // 2
     @test bernoulliB(60; msg=false) == -1215233140483755572040304994079820246041491 // 56786730
