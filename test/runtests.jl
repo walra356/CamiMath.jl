0897c8ad124defedeb1430459233c5c645a9bb37
Jook Walraven <walra356@planet.nl>
2023-02-02 15:38:51 +0100

    @test bigfactorial

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,7 @@ using Test
     @test bernoulliB_array(10) == [1 // 1, -1 // 2, 1 // 6, 0 // 1, -1 // 30, 0 // 1, 1 // 42, 0 // 1, -1 // 30, 0 // 1, 5 // 66]
     @test bernoulliB_array(big(8)) == Rational{BigInt}[1//1, -1//2, 1//6, 0//1, -1//30, 0//1, 1//42, 0//1, -1//30]
     @test bernoulliB(60; msg=false) == -1215233140483755572040304994079820246041491 // 56786730
-    @test (bernoulliB(60; msg=false) == bernoulliB_array(60; msg=false)[end]) == true
+    @test bernoulliB(60; msg=false) == bernoulliB_array(60; msg=false)[end]
+    @test @test bigfactorial(21; msg=false) == 51090942171709440000
 
 end
