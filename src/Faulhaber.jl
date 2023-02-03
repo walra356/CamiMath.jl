1eaac83b42d068c898ab8ebdb1095f66263b7976
Jook Walraven <walra356@planet.nl>
2023-02-03 17:59:26 +0100

    work on docs

--- src/Faulhaber.jl
+++ src/Faulhaber.jl
@@ -14,7 +14,7 @@ of degree `p`
 ```
 with vector elements
 ```math
-    c_0=0\ \ {\rm{and}}\ \c_j=\frac{1}{p}{\binom {p}{p-j}}B_{p-j},
+    c_0=0,\ \ c_j=\frac{1}{p}{\binom{p}{p-j}}B_{p-j},
 ```
 where ``j∈\{ 1,⋯\ p\}``. The ``B_0,⋯\ B_{p-1}`` are Bernoulli numbers
 (but with ``B_1=+\frac{1}{2}`` rather than ``-\frac{1}{2}``).
@@ -78,8 +78,6 @@ function faulhaber_polynomial(n::T, p::T) where {T<:Integer}
 
     Base.denominator(o) == 1 || error("Error: Faulhaber sum failed")
 
-    return Base.numerator(o)
-
 end
 
 # =================================== faulhaber_summation(n,p;T) ===============
@@ -87,7 +85,7 @@ end
 @doc raw"""
     faulhaber_summation(n::T, p::T) where {T<:Integer}
 
-Sum of powers of natural numbers ``1,⋯\ n`` as given by the *Faulhaber formula*
+Sum of the ``p^{th}`` powers of the first ``n`` natural numbers
 ```math
     \sum_{k=1}^{n}k^{p}=F(n,p+1).
 ```
