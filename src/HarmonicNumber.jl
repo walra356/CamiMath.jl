f50b01c7807e07e6b6beb10ee179fd6033e6077b
Jook Walraven <walra356@planet.nl>
2023-02-05 16:34:20 +0100

    codecov

--- src/HarmonicNumber.jl
+++ src/HarmonicNumber.jl
@@ -263,6 +263,7 @@ function harmonicNumber(n::T, p::Int; msg=true) where {T<:Integer}
         n = convert(Int, n)
         nc = p < 11 ? length(gl_harmon_Int[p]) : p < 18 ? 4 : p < 25 ? 3 : 0
         if n ≤ nc
+            println("hoi")
             o = T == Int ? gl_harmon_Int[p][n] : gl_harmon_BigInt[p][n]
         else
             o = _harmonicNumber_p_BigInt(n, nc, p)[end]
