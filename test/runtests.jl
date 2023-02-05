e968c4729805f3d51a2b3cd4c5c7036a724bd85c
Jook Walraven <walra356@planet.nl>
2023-02-05 20:49:21 +0100

    codecov

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,10 +15,12 @@ using Test
     @test bigfactorial(21; msg=false) == 51090942171709440000
     @test sum([sum(faulhaber_polynom(p; msg=false)) for p = 1:40]) == 40 // 1
     @test sum([sum(faulhaber_polynom(big(p); msg=false)) for p = 1:40]) == 40 // 1
+    #   ............................................................................
     @test faulhaber_polynomial(3, 6) == 276
     @test faulhaber_polynomial(5, 30; msg=false) == 186552813930161650665
+    @test faulhaber_polynomial(5, 36; msg=false) == (2911563687325667231369625)
     @test faulhaber_summation(3, 5) == 276
-#   ............................................................................
+    #   ............................................................................
     @test harmonicNumber(3, -5) == 276
     @test harmonicNumber(46) == (5943339269060627227 // 1345655451257488800)
     @test harmonicNumber(47; msg=false) == (280682601097106968469 // 63245806209101973600)
@@ -30,7 +32,7 @@ using Test
     @test typeof(harmonicNumber(big(8))) == Rational{BigInt}
     @test typeof(harmonicNumber(12, 3)) == Rational{Int}
     @test typeof(harmonicNumber(big(12), 3)) == Rational{BigInt}
-#   ............................................................................
+    #   ............................................................................
     @test harmonicNumber_array(8) == [1 // 1, 3 // 2, 11 // 6, 25 // 12, 137 // 60, 49 // 20, 363 // 140, 761 // 280]
     @test harmonicNumber_array(big(8)) == [1 // 1, 3 // 2, 11 // 6, 25 // 12, 137 // 60, 49 // 20, 363 // 140, 761 // 280]
     @test pascal_triangle(5) == [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1]]
