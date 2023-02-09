6b88f7fc38c39ba913eda66ca5656db953a6ea15
Jook Walraven <69215586+walra356@users.noreply.github.com>
2023-02-09 16:26:15 +0100

    work on docs

--- src/Faulhaber.jl
+++ src/Faulhaber.jl
@@ -170,7 +170,6 @@ of degree `p`
    c=[c_0,⋯\ c_p]
 ```
 where ``c_0=0,\ \ c_j=\frac{1}{p}{\binom{p}{p-j}}B_{p-j}``,
-```
 with ``j∈\{ 1,⋯\ p\}``. The ``B_{p-j}`` are Bernoulli numbers
 (but with ``B_1=+\frac{1}{2}`` rather than ``-\frac{1}{2}``).
 
