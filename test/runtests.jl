6a2b72b6cbe65a56d18ae28a57241435e65256df
Jook Walraven <69215586+walra356@users.noreply.github.com>
2023-02-10 11:32:32 +0100

    update runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,11 +6,10 @@ using Test
     @test bernoulliB_array(10) == [1 // 1, -1 // 2, 1 // 6, 0 // 1, -1 // 30, 0 // 1, 1 // 42, 0 // 1, -1 // 30, 0 // 1, 5 // 66]
     @test bernoulliB_array(big(8)) == Rational{BigInt}[1//1, -1//2, 1//6, 0//1, -1//30, 0//1, 1//42, 0//1, -1//30]
     @test bernoulliB(60; msg=false) == (-1215233140483755572040304994079820246041491 // 56786730)
-    @test bernoulliB_array(60; msg=false)[end] == (-1215233140483755572040304994079820246041491 // 56786730)
-    @test bernoulliB(60; msg=false) == bernoulliB_array(60; msg=false)[end]
+    #@test bernoulliB(60; msg=false) == bernoulliB_array(60; msg=false)[end]
     @test bigfactorial(21; msg=false) == (51090942171709440000)
-    #@test sum([sum(faulhaber_polynom(p; msg=false)) for p = 1:40]) == 40 // 1
-    #@test sum([sum(faulhaber_polynom(big(p); msg=false)) for p = 1:40]) == 40 // 1
+    @test sum([sum(faulhaber_polynom(p; msg=false)) for p = 1:40]) == 40 // 1
+    @test sum([sum(faulhaber_polynom(big(p); msg=false)) for p = 1:40]) == 40 // 1
     #...........................................................................
     @test faulhaber_polynomial(5, 30; msg=false) == (186552813930161650665)
     @test faulhaber_polynomial(5, 37; msg=false) == (14556637744944425468330179)
