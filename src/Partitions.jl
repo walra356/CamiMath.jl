66281e0d25b091c18c702a2e9bba711fe1cf191e
Jook Walraven <69215586+walra356@users.noreply.github.com>
2023-02-16 21:55:23 +0100

    work on docs

--- src/Partitions.jl
+++ src/Partitions.jl
@@ -20,38 +20,6 @@ function _canonical_partition(n::Int, m::Int)
 
 end
 
-@doc raw"""
-    canonical_partitions(n [[; header=false], reverse=true])
-
-The canonical partition in integers of the integer n
-header=true : unit patition included in output
-#### Examples:
-```
-canonical_partitions(6; header=true, reverse=false)
-6-element Array{Array{Int64,1},1}:
- [6]
- [5, 1]
- [4, 2]
- [3, 3]
- [2, 2, 2]
- [1, 1, 1, 1, 1, 1]
-canonical_partitions(6; header=true)
-6-element Array{Array{Int64,1},1}:
- [1, 1, 1, 1, 1, 1]
- [2, 2, 2]
- [3, 3]
- [4, 2]
- [5, 1]
- [6]
-canonical_partitions(6)
-5-element Array{Array{Int64,1},1}:
- [1, 1, 1, 1, 1, 1]
- [2, 2, 2]
- [3, 3]
- [4, 2]
- [5, 1]
-```
-"""
 function canonical_partitions(n::Int, m=0; header=true, reverse=true)
 
     h = header ? n : n - 1
@@ -107,57 +75,6 @@ function _restricted_partitions(o::Vector{Int}, n::Int, np::Int, cp::Vector{Vect
 
 end
 
-@doc raw"""
-    integer_partitions(n [[[,m]; transpose=false], count=false])
-
-`default`                      : The integer partitions of n
-`count`                        : The number of integer partitions of n
-`transpose` = `false` (`m` > 0): partitions restricted to maximum part m
-            = `true`  (`m` > 0): partitions restricted to maximum length m
-definitions:
-
-The integer partition of the positive integer `n` is a nonincreasing 
-sequence of positive integers p1, p2,... pk whose sum is `n`.
-The elements of the sequence are called the parts of the partition.
-#### Examples:
-```
-julia> integer_partitions(7)
-15-element Vector{Vector{Int64}}:
- [1, 1, 1, 1, 1, 1, 1]
- [2, 2, 2, 1]
- [2, 2, 1, 1, 1]
- [2, 1, 1, 1, 1, 1]
- [3, 3, 1]
- [3, 2, 2]
- [3, 2, 1, 1]
- [3, 1, 1, 1, 1]
- [4, 3]
- [4, 2, 1]
- [4, 1, 1, 1]
- [5, 2]
- [5, 1, 1]
- [6, 1]
- [7]
-
-julia> integer_partitions(7; count=true)
-15
-
-julia> integer_partitions(7, 4; count=true)
-3
-
-julia> integer_partitions(7, 4)
-3-element Vector{Vector{Int64}}:
- [4, 3]
- [4, 2, 1]
- [4, 1, 1, 1]
-
-julia> integer_partitions(7, 4; transpose=true)
-3-element Vector{Vector{Int64}}:
- [2, 2, 2, 1]
- [3, 2, 1, 1]
- [4, 1, 1, 1]
-```
-"""
 function integer_partitions(n::Int, m=0; transpose=false, count=false)
 
     cp = [canonical_partitions(m) for m = 1:n]
