e79009aeaa3a3f1e9a275dc644ad01d7af272c5d
Jook Walraven <69215586+walra356@users.noreply.github.com>
2023-02-20 19:05:06 +0100

    work on docs

--- test/runtests.jl
+++ test/runtests.jl
@@ -102,7 +102,7 @@ using Test
     @test polynom_product_expansion((1, -1, 1), (1, 1, -1, 1, 1, 1), 5) == [1, 0, -1, 3, -1, 1]
     @test polynom_power((1, 1, 1), 0) == (1)
     @test polynom_power((1, 1, 1), 1) == (1, 1, 1)
-    @test polynom_power((1, 1, 1), 2) == [1, 2, 3, 2, 1]
+    @test polynom_power((1, 1, 1), 2) == [1, 3, 6, 7, 6, 3, 1]
 
     @test texp(1 // 1, 0 // 1, 5) == 163 // 60
     @test texp(1, 0, 5) == 163 // 60
