f6a6ca799f6deee31f65fe12e78bc2ce72fbbe96
walra356 <walra356@planet.nl>
2024-11-20 13:24:17 +0100

    update licence in files

--- src/Toolbox.jl
+++ src/Toolbox.jl
@@ -1,8 +1,27 @@
 # SPDX-License-Identifier: MIT
 
+# Copyright (c) 2023 Jook Walraven <69215586+walra356@users.noreply.github.com> and contributors
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # ==============================================================================
 #                             Toolbox.jl
-#                      Jook Walraven - 23-2-2023
 # ==============================================================================
 
 # ------------------------------------------------------------------------------
@@ -185,6 +204,17 @@ function strRational(n::T) where T<:Union{Rational{}, Int, BigInt}
 end
 
 # ------------------------------------------------------------------------------
+#                                 fwd
+# ------------------------------------------------------------------------------
+@doc raw"""
+    fwd
+
+Singleton type indicating `forward` sense
+"""
+struct fwd
+end
+
+# ------------------------------------------------------------------------------
 #                            isforward(sense) 
 # ------------------------------------------------------------------------------
 
@@ -204,6 +234,17 @@ strErr = "Error: invalid sense (options: fwd, bwd)"
 
 return sense === fwd ? true : sense === bwd ? false : error(strErr)
 
+end
+
+# ------------------------------------------------------------------------------
+#                                bwd
+# ------------------------------------------------------------------------------
+@doc raw"""
+    bwd
+
+Singleton type indicating `backward` sense
+"""
+struct bwd
 end
 
 # ------------------------------------------------------------------------------
@@ -229,6 +270,17 @@ return sense === bwd ? true : sense === fwd ? false : error(strErr)
 end
 
 # ------------------------------------------------------------------------------
+#                                  reg
+# ------------------------------------------------------------------------------
+@doc raw"""
+    reg
+
+Singleton type indicating `regular` ordering
+"""
+struct reg
+end
+
+# ------------------------------------------------------------------------------
 #                            isregular(sense) 
 # ------------------------------------------------------------------------------
 
@@ -248,6 +300,18 @@ strErr = "Error: invalid sense (options: reg, rev)"
 
 return sense === reg ? true : sense === rev ? false : error(strErr)
 
+end
+
+# ------------------------------------------------------------------------------
+#                               rev
+# ------------------------------------------------------------------------------
+
+@doc raw"""
+    rev
+
+Singleton type indicating `reverse` ordering
+"""
+struct rev
 end
 
 # ------------------------------------------------------------------------------
