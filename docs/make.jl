9711c470d3c0655f8eea742b0ac4432e7ff39728
walra356 <walra356@planet.nl>
2025-02-06 21:43:59 +0100

    work on docs

--- docs/make.jl
+++ docs/make.jl
@@ -13,11 +13,11 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
-        "Documentation" => "/pages/documentation.md",
-        "Polynomials" => "/pages/polynomials.md",
-        "VectorCoupling" => "/pages/vectorcoupling.md",
-        "Toolbox" => "/pages/toolbox.md",
-        "Index" => "/pages/index.md"
+        "Documentation" => "pages/documentation.md",
+        "Polynomials" => "pages/polynomials.md",
+        "VectorCoupling" => "pages/vectorcoupling.md",
+        "Toolbox" => "pages/toolbox.md",
+        "Index" => "pages/index.md"
     ]
 )
 
