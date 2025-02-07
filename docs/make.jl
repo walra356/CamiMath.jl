0c7cc0339cf7bd721340e86f26e5a879d38e0f44
walra356 <walra356@planet.nl>
2025-02-07 10:16:27 +0100

    work on docs

--- docs/make.jl
+++ docs/make.jl
@@ -12,13 +12,19 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
-        "Manual" => Any[
         "Documentation" => "pages/documentation.md",
         "VectorCoupling" => "pages/vectorcoupling.md",
         "Toolbox" => "pages/toolbox.md",
         "Index" => "pages/index.md"
-        ]
-    ]
+    ],
+    Depth=1,
+    doctest=true,
+    doctestargs=["--project"],
+    doctestcoverage=true,       # coverage report           (default: false)                                                
+    doctestcoverageargs=["--project"],
+    doctestcoverageformat=:lcov, # coverage report format    (default: :lcov)
+    doctestcoveragefile="lcov.info", # coverage report file      (default: "lcov.info")
+    doctestcoverageexclude=["src/CamiMath.jl"], # coverage report exclude   (default: [])               
 )
 
 deploydocs(;
